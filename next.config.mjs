3f7978a3dd0ea11991a28be2999a10fc14e35fb8
yagnesh0312 <yagnesh03122002@gmail.com>
2024-12-18 13:51:30 +0530

    Add GitHub Actions workflow for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['github.com', "prexakanabar.github.io","img.icons8.com"],
+        domains: ['github.com', "prexakanabar.github.io", "img.icons8.com"],
     },
+    output: 'export',
+    basePath: '/p_dev', // Replace with your repository name
+    assetPrefix: '/p_dev/',
 };
 
 export default nextConfig;
