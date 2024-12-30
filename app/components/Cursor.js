ed8bcc624270ffe369404fbf7fb299092e49a4e5
yagnesh jariwala <yagnesh03122002@gmail.com>
2024-12-30 18:10:36 +0530

    add cursor beautifull

--- app/components/Cursor.js
+++ app/components/Cursor.js
@@ -43,13 +43,13 @@ function Cursor() {
             <div
                 className="absolute z-[50] transition-transform duration-1000 ease-out cursor-border"
                 style={{
-                    transform: `translate3d(${position.x}px, ${position.y}px, ${position.z}px) scale(${isLinkHovered ? 1.2 : 1})`,
+                    transform: `translate3d(${position.x+5}px, ${position.y+5}px, ${position.z+5}px) scale(${isLinkHovered ? 1.2 : 1})`,
                     width: '25px',
                     height: '25px',
                     borderRadius: '50%',
-                    backgroundColor: 'rgba(255,255,255,0.3)',
-                    boxShadow: '0px 0px 50px 2px rgba(59,130,246,0.5)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(59,130,246,0.5))'
+                    backgroundColor: 'rgba(16,185,129,0.3)',
+                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.5)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
                 }}
             >
                 <div
@@ -59,13 +59,13 @@ function Cursor() {
             <div
                 className="absolute z-[55] transition-transform duration-500 ease-out"
                 style={{
-                    transform: `translate3d(${position.x + 15}px, ${position.y + 15}px, ${position.z + 15}px) scale(${isLinkHovered ? 1.25 : 1})`,
+                    transform: `translate3d(${position.x+5}px, ${position.y+5}px, ${position.z+5}px) scale(${isLinkHovered ? 1.25 : 1})`,
                     width: '10px',
                     height: '10px',
                     borderRadius: '50%',
-                    backgroundColor: 'rgba(255,255,255,0.3)',
-                    boxShadow: '0px 0px 50px 2px rgba(59,130,246,0.5)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(59,130,246,0.5))'
+                    backgroundColor: 'rgba(16,185,129,0.3)',
+                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.5)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
                 }}
             >
                 <div
@@ -73,14 +73,14 @@ function Cursor() {
                 />
             </div>
             <div
-                className={`absolute z-[60] transition-transform duration-150 ease-out mix-blend-difference cursor-root ${isLinkHovered ? 'bg-blue-500 shadow-[0_0_50px_2px_rgb(59,130,246)]' : 'bg-white'}`}
+                className={`absolute z-[60] transition-transform duration-150 ease-out mix-blend-difference cursor-root ${isLinkHovered ? 'bg-green-500 shadow-[0_0_50px_2px_rgb(16,185,129)]' : 'bg-white'}`}
                 style={{
-                    transform: `translate3d(${position.x + 20}px, ${position.y + 20}px, ${position.z + 20}px) scale(${isLinkHovered ? 1.3 : 1})`,
+                    transform: `translate3d(${position.x+5}px, ${position.y+5}px, ${position.z+5}px) scale(${isLinkHovered ? 1.3 : 1})`,
                     width: '10px',
                     height: '10px',
                     borderRadius: '50%',
-                    boxShadow: '0px 0px 50px 2px rgba(59,130,246,0.5)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(59,130,246,0.5))'
+                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.5)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
                 }}
             >
                 <div
