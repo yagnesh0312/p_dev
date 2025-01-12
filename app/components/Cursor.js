e7a7b679be36b3c0239ed5520d5e0816ebc005f2
yagnesh jariwala <yagnesh03122002@gmail.com>
2025-01-12 10:49:15 +0530

    change to purple theme

--- app/components/Cursor.js
+++ app/components/Cursor.js
@@ -47,9 +47,9 @@ function Cursor() {
                     width: '25px',
                     height: '25px',
                     borderRadius: '50%',
-                    backgroundColor: 'rgba(16,185,129,0.3)',
-                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.9)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
+                    backgroundColor: 'rgba(255,110,199,0.3)',
+                    boxShadow: '0px 0px 50px 2px rgba(159,93,227,0.9)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(243,168,131,0.5))'
                 }}
             >
                 <div
@@ -65,9 +65,9 @@ function Cursor() {
                     width: '10px',
                     height: '10px',
                     borderRadius: '50%',
-                    backgroundColor: 'rgba(16,185,129,0.3)',
-                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.9)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
+                    backgroundColor: 'rgba(255,110,199,0.3)',
+                    boxShadow: '0px 0px 50px 2px rgba(159,93,227,0.9)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(243,168,131,0.5))'
                 }}
             >
                 <div
@@ -75,14 +75,14 @@ function Cursor() {
                 />
             </div>
             <div
-                className={`absolute z-[600] transition-transform duration-150 ease-out mix-blend-difference cursor-root ${isLinkHovered ? 'bg-green-500 shadow-[0_0_50px_2px_rgb(16,185,129)]' : 'bg-white'}`}
+                className={`absolute z-[600] transition-transform duration-150 ease-out mix-blend-difference cursor-root ${isLinkHovered ? 'bg-#ff6ec7 shadow-[0_0_50px_2px_rgb(159,93,227)]' : 'bg-white'}`}
                 style={{
                     transform: `translate3d(${position.x + 5}px, ${position.y + 5}px, ${position.z + 5}px) scale(${isLinkHovered ? 1.3 : 1})`,
                     width: '10px',
                     height: '10px',
                     borderRadius: '50%',
-                    boxShadow: '0px 0px 50px 2px rgba(16,185,129,0.9)',
-                    filter: 'drop-shadow(0px 0px 50px rgba(16,185,129,0.5))'
+                    boxShadow: '0px 0px 50px 2px rgba(159,93,227,0.9)',
+                    filter: 'drop-shadow(0px 0px 50px rgba(243,168,131,0.5))'
                 }}
             >
                 <div
